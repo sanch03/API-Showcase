f241da670651711b0a3d629c8558d185512cd014
Sanchit <sanchitormoney@gmail.com>
2020-04-15 13:06:09 +1000

    apidetails

--- dist/js/apidetails.js
+++ dist/js/apidetails.js
@@ -1,19 +1,24 @@
 function getdetails(name) {
-    $.ajax({
-        //data: {day: day, month: month, year: year},
-        url: 'https://api.publicapis.org/entries?title='+name,
-        method: 'POST', // or GET
-             success: function(msg) {
-                var t = msg;
-               console.log(t);
-                 
+    var e = new XMLHttpRequest;
+    e.onreadystatechange = function() {
+        if (4 == e.readyState && 200 == e.status) {
+            var t = e.responseText;
+        console.log("hi");
 
-                 
-             }
-    });    
-
-
-
-
+            console.log(t);
+var obj = JSON.parse(t);
+console.log(obj.entries[0]);
+//var val = JSON.parse(obj[0]);
+//console.log(val);
+document.getElementById("about").innerHTML = obj.entries[0].Description;
+document.getElementById("auth").innerHTML = obj.entries[0].Auth;
+document.getElementById("https").innerHTML = obj.entries[0].HTTPS;
+document.getElementById("cors").innerHTML = obj.entries[0].Cors;
+document.getElementById("url").href = obj.entries[0].Link;
+        }   
+    }
+    ,
+    e.open("GET", "https://api.publicapis.org/entries?title="+name, !0),
+    e.send(null)
 
 }
