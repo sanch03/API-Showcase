82b7372c2b3d496e2770c29eae3202a44c4e75b5
Nikith Kaluwitharana <nikithkalu@gmail.com>
2020-04-18 21:29:37 +1000

    kappa pride

--- dist/js/Animals/PlaceGOAT.js
+++ dist/js/Animals/PlaceGOAT.js
@@ -1,1 +1,20 @@
-highiq("http://placegoat.com/200");
+var e =  document.getElementById('tempdiv');
+if (typeof(e) != 'undefined' && e != null)
+{
+    var el = document.getElementById("tempdiv");
+    el.parentNode.removeChild(el);
+
+  // Exists.
+}
+var div = document.createElement("div");
+div.id = "tempdiv";
+// The following is the HTML for "Usage Examples"
+div.innerHTML = `<pre class="prettyprint" id="code">img id="image" src="http://placegoat.com/200"</pre>`;
+var element = document.getElementById("cexample");
+element.appendChild(div);
+//$.getScript("/dist/js/run_prettify.js");
+refreshcode();         
+
+var outdiv = document.getElementById("output");           
+
+outdiv.innerHTML = '<img id="image" src="http://placegoat.com/200">'
