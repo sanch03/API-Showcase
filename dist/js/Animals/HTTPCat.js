66584ab65cb4d7aa999d2b59e833bd06705f223f
Sanchit <sanchitormoney@gmail.com>
2020-04-18 21:19:31 +1000

    HTTPCat & AniList

--- dist/js/Animals/HTTPCat.js
+++ dist/js/Animals/HTTPCat.js
@@ -1,3 +1,26 @@
-highiq("https://http.cat/200");
+//highiq("https://http.cat/200");
+//Try working on Dogs now
+var e =  document.getElementById('tempdiv');
+if (typeof(e) != 'undefined' && e != null)
+{
+    var el = document.getElementById("tempdiv");
+    el.parentNode.removeChild(el);
+
+  // Exists.
+}
+var div = document.createElement("div");
+div.id = "tempdiv";
+// The following is the HTML for "Usage Examples"
+div.innerHTML = `<pre class="prettyprint" id="code">img id="image" src="https://http.cat/200"</pre>`;
+var element = document.getElementById("cexample");
+element.appendChild(div);
+//$.getScript("/dist/js/run_prettify.js");
+refreshcode();         
+
+var outdiv = document.getElementById("output");           
+
+outdiv.innerHTML = '<img id="image" src="https://http.cat/200">';
+
+   
 
 
--- dist/js/Anime/AniList.js
+++ dist/js/Anime/AniList.js
@@ -11,22 +11,35 @@ var div = document.createElement("div");
 div.id = "tempdiv";
 // The following is the HTML for "Usage Examples"
 div.innerHTML = `<pre class="prettyprint" id="code">some other code I havent put in yet</pre>
-<pre class="prettyprint" id="code">$.ajax({
-    var url = "https://api.thecatapi.com/v1/breeds" // set accordingly
+<pre class="prettyprint" id="code">
+var query = \`
+query ($id: Int) { # Define which variables will be used in the query (id)
+  Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+    id
+    title {
+      romaji
+      english
+      native
+    }
+  }
+}
+\`;
+
+// Define our query variables and values that will be used in the query request
+var variables = {
+    id: 15125
+};
+
+$.ajax({
+    data: {query: query, variables: variables},
+    var url = "https://graphql.anilist.co" // set accordingly
     url: url,
-    method: 'GET',
+    method: 'POST',
          success: function(msg) {
             var obj = msg;
             console.log(obj);         
          }
 });</pre>
-
-
-
-
-
-
-
 
 
 
