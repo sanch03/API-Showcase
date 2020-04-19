9fffe3e1894b73b4e4a0ea0a82162b3d26d658c2
Nikith Kaluwitharana <nikithkalu@gmail.com>
2020-04-19 10:51:11 +1000

    RandomFox

---
+++ dist/js/Animals/RandomFox.js
@@ -0,0 +1,20 @@
+var e =  document.getElementById('tempdiv');
+if (typeof(e) != 'undefined' && e != null)
+{
+    var el = document.getElementById("tempdiv");
+    el.parentNode.removeChild(el);
+
+  // Exists.
+}
+var div = document.createElement("div");
+div.id = "tempdiv";
+// The following is the HTML for "Usage Examples"
+div.innerHTML = `<pre class="prettyprint" id="code">img id="image" src="http://randomfox.ca/images/12.jpg"</pre>`;
+var element = document.getElementById("cexample");
+element.appendChild(div);
+//$.getScript("/dist/js/run_prettify.js");
+refreshcode();         
+
+var outdiv = document.getElementById("output");           
+
+outdiv.innerHTML = '<img id="image" src="http://randomfox.ca/images/12.jpg">'
