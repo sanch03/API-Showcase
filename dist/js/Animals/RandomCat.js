cb6ea3c536a4e1d597813042b9ae093314360c8c
Nikith Kaluwitharana <nikithkalu@gmail.com>
2020-04-19 10:35:42 +1000

    RandomCat Fix

--- dist/js/Animals/RandomCat.js
+++ dist/js/Animals/RandomCat.js
@@ -1,1 +1,20 @@
-highiq("https://aws.random.cat/meow");
+var e =  document.getElementById('tempdiv');
+if (typeof(e) != 'undefined' && e != null)
+{
+    var el = document.getElementById("tempdiv");
+    el.parentNode.removeChild(el);
+
+  // Exists.
+}
+var div = document.createElement("div");
+div.id = "tempdiv";
+// The following is the HTML for "Usage Examples"
+div.innerHTML = `<pre class="prettyprint" id="code">img id="image" src="https://purr.objects-us-east-1.dream.io/i/img_20170904_234526.jpg"</pre>`;
+var element = document.getElementById("cexample");
+element.appendChild(div);
+//$.getScript("/dist/js/run_prettify.js");
+refreshcode();         
+
+var outdiv = document.getElementById("output");           
+
+outdiv.innerHTML = '<img id="image" src="https://purr.objects-us-east-1.dream.io/i/img_20170904_234526.jpg">'
